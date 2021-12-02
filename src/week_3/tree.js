18192af6b3e0d5520e4bdf697701429e5d9667b6
bsh00699 <525534397@qq.com>
2021-12-02 23:37:39 +0800

    feat: 递归与分治处理树遍历、括号生成问题

--- src/week_3/tree.js
+++ src/week_3/tree.js
@@ -52,3 +52,83 @@ var maxDepth = function (root) {
   // 返回左右子树高度的最大值
   return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1
 };
+
+/**
+ * LeetCode-94. 二叉树的中序遍历
+ * 方法：递归
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root) {
+  const ans = []
+  const dfs = (root) => {
+    if (root === null) return
+    // 中序遍历 左 --> 根 --> 右
+    dfs(root.left)
+    ans.push(root.val)
+    dfs(root.right)
+  }
+  dfs(root)
+  return ans
+};
+
+/**
+ * LeetCode-94. 二叉树的中序遍历
+ * 方法：①递归 ②迭代
+ */
+/**
+ * // Definition for a Node.
+ * function Node(val, children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+/**
+ * @param {Node|null} root
+ * @return {number[]}
+ */
+var preorder = function (root) {
+  // 递归实现
+  const ans = []
+  const dfs = (root) => {
+    // 边界条件
+    if (root === null) return
+    // 先序遍历：先跟 --> 所有孩子
+    ans.push(root.val)
+    // 遍历每个孩子节点
+    for (const node of root.children) {
+      dfs(node)
+    }
+  }
+  dfs(root)
+  return ans
+};
+
+var preorder = function (root) {
+  // 迭代法实现
+  const ans = []
+  if (root === null) return ans
+  // 利用栈结构；放数据的时候 倒着放，比如 取出是 3 2 4，放的时候要 4 2 3
+  const stack = []
+  stack.push(root)
+  while (stack.length) {
+    const node = stack.pop()
+    ans.push(node.val)
+    // 入栈
+    const len = node.children.length
+    for (let i = len - 1; i >= 0; i--) {
+      stack.push(node.children[i])
+    }
+  }
+  return ans
+};
