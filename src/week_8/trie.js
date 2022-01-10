86326214957e6f94e5032ef0cea8412b39084489
bsh00699 <525534397@qq.com>
2022-01-10 23:05:41 +0800

    feat: 字典树+df处理单词搜索

--- src/week_8/trie.js
+++ src/week_8/trie.js
@@ -81,3 +81,126 @@ var findCircleNum = function (isConnected) {
   }
   return ans
 };
+
+/**
+ * LeetCode-212. 单词搜索 II
+ */
+/**
+ * @param {character[][]} board
+ * @param {string[]} words
+ * @return {string[]}
+ */
+// 字典树插入
+const insert = (word, curr) => {
+  // root: [count, child] => [0, {}]
+  for (let ch of word) {
+    if (!curr[1][ch]) {
+      curr[1][ch] = [0, {}]
+    }
+    curr = curr[1][ch]
+  }
+  curr[0] = curr[0] + 1
+}
+
+var findWords = function (board, words) {
+  // 方法: Trie字典数 + DFS 
+  // 1.建立Trie, 插入words
+  let root = [0, {}] // root: [count, child] => [0, {}]
+  for (let word of words) {
+    insert(word, root)
+  }
+  // 2.DFS 枚举每个起点 搜索
+  const m = board.length
+  const n = board[0].length
+  const dr = [[-1, 0], [0, -1], [0, 1], [1, 0]]
+  const visit = new Array(m).fill(0).map(() => {
+    return new Array(n).fill(false)
+  })
+  let str = []
+  let ans = []
+  const dfs = (board, x, y, curr) => {
+    const ch = board[x][y]
+    if (!curr[1][ch]) return
+    let next = curr[1][ch]
+    str.push(ch)
+    // 判断是不是找到
+    if (curr[0] > 0) {
+      ans.push(str.join(''))
+    }
+    // 向四个方向查找
+    for (const [dx, dy] of dr) {
+      const nx = x + dx
+      const ny = y + dy
+      // 是否越界
+      if (nx < 0 || ny < 0 || nx >= m || ny >= n) {
+        continue
+      }
+      if (visit[nx][ny]) continue
+      visit[nx][ny] = true
+      dfs(board, nx, ny, next)
+      visit[nx][ny] = false
+    }
+    str.pop()
+  }
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      visit[i][j] = true
+      dfs(board, i, j, root)
+      visit[i][j] = false
+    }
+  }
+  return ans
+
+};
+// 或者这样
+var findWords = function (board, words) {
+  /**
+  * build trie --> traverse board --> DFS
+  */
+  const res = [], h = board.length, w = board[0].length;
+  // 构建 tire
+  const getTrie = words => {
+    let root = Object.create(null);
+    for (const word of words) {
+      let node = root;
+      for (let c of word) {
+        if (!node[c]) {
+          node[c] = Object.create(null);
+        }
+        node = node[c];
+      }
+      node.word = word;
+    }
+    return root;
+  }
+
+  // DFS 深度优先搜素
+  const search = (trie, i, j) => {
+    // 到结尾
+    if (trie.word) {
+      res.push(trie.word);
+      trie.word = null;
+    }
+    // 边界条件
+    if (i < 0 || j < 0 || i >= h || j >= w) return;
+    // 不在字典树中，返回
+    if (!trie[board[i][j]]) return;
+
+    let prefixChar = board[i][j];
+    board[i][j] = "#"; // mark visited
+    search(trie[prefixChar], i, j - 1);
+    search(trie[prefixChar], i, j + 1);
+    search(trie[prefixChar], i - 1, j);
+    search(trie[prefixChar], i + 1, j);
+    board[i][j] = prefixChar; // restore
+  }
+
+  // traverse board
+  const trie = getTrie(words);
+  for (let i = 0; i < h; i++) {
+    for (let j = 0; j < w; j++) {
+      search(trie, i, j)
+    }
+  }
+  return res;
+};
