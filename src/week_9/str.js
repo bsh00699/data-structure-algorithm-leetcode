63b94a5046e5c8727b802b91b8cdeee23d8eac7f
bsh00699 <525534397@qq.com>
2022-01-13 23:53:23 +0800

    feat: Rabin-Karp 字符串哈希算法 实现 strStr

--- src/week_9/str.js
+++ src/week_9/str.js
@@ -34,4 +34,40 @@ var myAtoi = function (s) {
     }
     return sign * val
 };
-test
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+    // 
+    // b = 131 p = Math.pow(10, 9) + 7
+    const b = 131
+    const p = 1e9 + 7
+    //   const p = Math.pow(10, 9) + 7
+    const n = haystack.length
+    const m = needle.length
+    const H = new Array(n + 1).fill(0)
+    // 字符串转number a = 1, b = 2...z = 26
+    // 1.对haystack 取hash => H
+    for (let i = 1; i <= n; i++) {
+        H[i] = (H[i - 1] * b + (haystack[i - 1] + 1)) % p
+    }
+    // 2.对needle取hash => Hneedle
+    let Hneedle = 0
+    let powBM = 1
+    for (let j = 0; j < m; j++) {
+        Hneedle = (Hneedle * b + (needle[j] + 1)) % p
+        powBM = powBM * b % p
+    }
+    // 3.haystack中每一个长为m的字串和Hneedle 比较
+    for (let l = 1; l <= n - m + 1; l++) {
+        let r = l + m - 1
+        // s[l ~ r]的hash值 === Hneedle ?
+        if (((H[r] - H[l - 1] * powBM) * p + p) % p === Hneedle) {
+            return l - 1
+        }
+    }
+    return -1
+};
