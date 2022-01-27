5fc1d7f00783c3424c607c9935a5c11465c6f5e5
bsh00699 <525534397@qq.com>
2022-01-27 23:37:29 +0800

    feat: 位运算处理1.位1个数2.pow(x, n)问题

---
+++ src/week_10/bitOpt.js
@@ -0,0 +1,58 @@
+/**
+ * LeetCode-191. 位1的个数
+ */
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function (n) {
+  let cnt = 0
+  let i = 0
+  while (i < 32) {
+    if ((n & 1) === 1) cnt++
+    n = n >> 1
+    i++
+  }
+  return cnt
+};
+
+/**
+ * LeetCode-50. Pow(x, n)
+ * 方法: 快速幂
+ */
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function (x, n) {
+  if (n === 0) return 1
+  if (n < 0) return 1 / myPow(x, -n)
+  let temp = x
+  let ans = 1.0
+  while (n > 0) {
+    if (n & 1) ans = ans * temp
+    temp = temp * temp
+    n = n >> 1
+  }
+  return ans
+};
+
+var myPow = function (x, n) {
+  if (n < 0) {
+    n = Math.abs(n)
+    x = 1 / x
+  }
+  if (n === 0) return 1
+  if (n === 1) return x
+  let divisor = 1
+  while (n > 1) {
+    if (n & 1) {
+      divisor *= x
+      n++
+    }
+    x = x * x
+    n >>>= 1
+  }
+  return x / divisor
+};
